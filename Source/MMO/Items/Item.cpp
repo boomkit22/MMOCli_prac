ec709926366a94c35eaef0fd5a32e01ee2d1d830
boomkit22 <boomkit22@gmail.com>
2024-06-22 15:40:38 +0900

    add item

---
+++ Source/MMO/Items/Item.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Item.h"
+
+AItem::AItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
