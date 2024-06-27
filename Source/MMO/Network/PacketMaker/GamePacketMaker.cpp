b03b5d3b7df4be9f068c4d11776b6f2df3f2f3b0
boomkit22 <boomkit22@gmail.com>
2024-06-27 17:34:31 +0900

    캐릭터 선택창 띄우는거 성공

--- Source/MMO/Network/PacketMaker/GamePacketMaker.cpp
+++ Source/MMO/Network/PacketMaker/GamePacketMaker.cpp
@@ -13,7 +13,9 @@ void GamePacketMaker::MP_CS_REQ_LOGIN(CPacket* Packet, int64& accountNo)
 	Header._randKey = rand();
 
 	Packet->PutData((char*)&Header, sizeof(NetHeader));
-	*Packet << accountNo;
+	uint16 type = PACKET_CS_GAME_REQ_LOGIN;
+
+	*Packet << type << accountNo;
 
 	uint16 len = (uint16)(Packet->GetDataSize() - sizeof(NetHeader));
 	FMemory::Memcpy(Packet->GetBufferPtr() + NET_HEADER_SIZE_INDEX, (void*)&len, sizeof(uint16));
